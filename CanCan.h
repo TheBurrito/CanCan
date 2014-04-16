c12b1e1698fc09fc13af4ef87d325584f2d097fe
DBommarito <dbommarito@ubuntu.ubuntu-domain>
2014-04-15 19:34:52 -0500

    Removed state machine code

--- CanCan.h
+++ CanCan.h
@@ -15,22 +15,6 @@ typedef enum {
 	eStateEnd
 } CanCanState;
 
-CanCanSoccer();
-~CanCanSoccer();
-
-//Pushing a state makes it the new current state.
-//The existing current state will be revisited in the future
-void pushState(State s);
-
-//Prematurely ends the current state. The state returned is the new current
-//state that will be active on the next call to update().
-State popState();
-
-//returns the current state of the CanCan state machine
-State currentState();
-
-bool update();
-
 } //namespace CanCan
 
 #endif //CanCan_h_
